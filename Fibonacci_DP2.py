731aea233f86b6d743716cdbb46b44779180d068
Chin-hung Yeh <chinhung.yeh@aa.com>
2022-05-19 19:56:24 -0500

    added fibonacci DP

---
+++ Fibonacci_DP2.py
@@ -0,0 +1,36 @@
+#%%
+def fib(n):
+
+    def __init_memo():
+        global memo
+        memo = []
+        for i in range(n+1):
+            memo.append(-1)
+
+    if 'memo' not in globals():
+        __init_memo()
+
+    if len(memo) <= n+1 :
+        for i in range(len(memo), n+1):
+            memo.append(-1)
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    if memo[n] != -1:
+        return memo[n]
+
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+
+# for i in range(10):
+#     print(fib(i))
+
+# %%
+%%timeit
+if __name__ == '__main__':
+    n = int(input())
+    print(fib(n))
