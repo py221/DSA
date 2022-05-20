3f1a6700375ebfa3b640e412530181797a9d1d90
Chin-hung Yeh <chinhung.yeh@aa.com>
2022-05-20 00:07:46 -0500

    added high fib mod w pisano period

--- week_2/pisano_length.py
+++ week_2/pisano_length.py
@@ -6,7 +6,7 @@ def pisano_length(m):
     if(fib(i+1)%m!=1):
         while(fib(i+1)%m!=1):
             i+=i
-    print("The pisano length for mod {} is: {}".format(m,i))
+    # print("The pisano length for mod {} is: {}".format(m,i))
     return(i)
 def fib(n):
     a,b=0,1
@@ -16,4 +16,9 @@ def fib(n):
         for i in range(2,n+1):
             b,a=a+b,b
     return(b)
+
+# %%
+n,m=map(int,input().split())
+remainder=n%pisano_length(m)
+print(fib(remainder)%m)
 # %%
